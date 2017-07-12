a40368ac93898f5afef389643f4d59a4b7b1d464
gabe <gpmora10@gmail.com>
2017-07-12 00:08:13 -0700

    Add select all query

---
+++ compact/database-stuff/queries.py
@@ -0,0 +1,17 @@
+import os, sqlite3
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+database = os.path.join(os.path.dirname(base_dir), 'db.sqlite3')
+
+def select_all_from_yield():
+	conn = sqlite3.connect(database)
+	sql = ''' SELECT * FROM yield'''
+	cur = conn.cursor()
+	cur.execute(sql)
+
+	rows = cur.fetchall()
+
+	for row in rows:
+		print(row)
+
+select_all_from_yield()
