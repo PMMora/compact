cd3aeb10ecb9cba8646c708d167a9f8096c113a4
justbreakingstuff <kdheard@uw.edu>
2017-07-31 16:27:43 +0000

    Got the view for the home page working, and added content to models.py

--- compact/compact/urls.py
+++ compact/compact/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'compact.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	#url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
+    url(r'^',include('compactapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- compact/compactapp/views.py
+++ compact/compactapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+
 # Create your views here.
+def home_screen(request):
+    #emplate = loader.get_template('base.html')
+    #context = RequestContext(request, {'posts': posts})
+    #body = template.render()
+    return HttpResponse(base.html, content_type="text/html")
+
+def results_screen(request):
+    pass
+
+def error_screen(request):
+    pass
