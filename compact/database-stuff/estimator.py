cc770e2dbb80b90127d69223c54e61844a596095
gabe <gpmora10@gmail.com>
2017-08-08 08:18:55 -0700

    Updated estimator

--- compact/database-stuff/estimator.py
+++ compact/database-stuff/estimator.py
@@ -7,7 +7,7 @@ conn = sqlite3.connect(database)
 def get_non_disclosed_rows(conn):
     sql = '''SELECT fips_id, naics_id, year, establishments, description
                 FROM yield JOIN industry ON naics = naics_id
-                WHERE disclosure_code = 'N' AND own_code = '5' LIMIT 316'''
+                WHERE disclosure_code = 'N' AND own_code = '5' '''
     cur = conn.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
@@ -24,7 +24,7 @@ def get_totals_of_non_disclosed_naics(conn, gen_naics, year, area):
 
 def get_totals_of_disclosed_rows(conn, gen_naics, year, area):
     sql = '''SELECT fips_id, naics_id, establishments, employees, wages FROM yield
-                WHERE naics_id LIKE '{}%' and year = {} and fips_id = {} and own_code='5' '''.format(gen_naics, year, area)
+                WHERE naics_id LIKE '{}_' and year = {} and fips_id = {} and own_code='5' '''.format(gen_naics, year, area)
     cur = conn.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
@@ -54,9 +54,9 @@ with conn:
             if line[3]==0 or line[4] == 0:
                 continue
             else:
-                #print('BEGIN NEW ENTRY')
-                #print(row)
-                #print(line)
+                print('BEGIN NEW ENTRY')
+                print(row)
+                print(line)
 
                 total_employees = int(line[3])
                 total_wages = int(line[4])
@@ -65,6 +65,9 @@ with conn:
 
                 undisclosed_rows = 0
 
+                disc_employees = 0
+                disc_wages =0
+
                 for disclosed_row in total_disclosed[1:]:
                     if disclosed_row[3] == 0 and disclosed_row[4] ==0:
                         undisclosed_rows += 1
@@ -72,11 +75,17 @@ with conn:
                     employees = disclosed_row[3]
                     wages = disclosed_row[4]
 
+                    disc_employees += disclosed_row[3]
+                    disc_wages += disclosed_row[4]
+
+
                     total_employees -= int(employees)
                     total_wages -= int(wages)
 
                 est_employees = round(total_employees/undisclosed_rows)
                 est_wages = round(total_wages/undisclosed_rows)
-                #print([total_employees,total_wages])
-        #print([naics, year, area, est_employees, est_wages])
-    update_non_disclosed_row(conn, str(naics), str(year), str(area), str(est_wages), str(est_employees))
+
+                print([disc_employees, disc_wages])
+                print([total_employees,total_wages])
+        print([naics, year, area, est_employees, est_wages])
+    #update_non_disclosed_row(conn, str(naics), str(year), str(area), str(est_wages), str(est_employees))
