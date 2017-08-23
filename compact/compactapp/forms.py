7e5f1504317929d13b729de0c281a28414b0dd93
Kayla Heard <kdheard@uw.edu>
2017-08-23 00:31:09 +0000

    Refactored templates to use Django forms as opposed to html forms

---
+++ compact/compactapp/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from compactapp.models import Area
+import xlrd
+
+class SimpleAnalysis(forms.Form):
+    """"Provides the user with the Simple Analysis form."""
+
+    areas = Area.objects.values('name')
+    area_list = list(entry['name'] for entry in areas)
+    area_list.insert(0, 'US')
+    area_number = list(range(0, len(area_list)))
+    area_list = zip(area_number, area_list)
+    #Gets all Area objects and grabs the list of state names.
+    #Appends the value "US" as this is not in the database, but is a valid input.
+    #Zips all state names into a list of tuples so Django Forms can consume it.
+
+    industry_file_location = "FinalNAICStoIOMatch.xlsx"
+    workbook = xlrd.open_workbook(industry_file_location)
+    sheet = workbook.sheet_by_name('NAICS to Sector')
+    sector_ID = list(item for item in sheet.col_values(0))
+    sector_description = list(item for item in sheet.col_values(1))
+    sector_NAICS = list(item for item in sheet.col_values(3))
+    #Gets all sector IDs, NAICS, and industry names from the excel sheet provided by Eric.
+
+    industries = ["NAICS Code: {} - {}".format(int(NAICS), desc) for NAICS, desc in zip(sector_NAICS, sector_description)]
+    industries = zip(sector_ID, industries)
+    #Formats the menu item choices for Industries and zips them into a list of tuples.
+
+
+    industry = forms.ChoiceField(choices=industries)
+    state = forms.ChoiceField(choices=area_list)
+
+class AdvancedAnalysis(forms.Form):
+
+    areas = Area.objects.values('name')
+    area_list = list(entry['name'] for entry in areas)
+    area_list.insert(0, 'US')
+    area_number = list(range(0, len(area_list)))
+    area_list = zip(area_number, area_list)
+    # Extremely unpythonic repetition of above code, however, Flask will not render the state list if I attempt to reuse the same code, because reasons
+
+    naics = forms.CharField(min_length=3, max_length=6, initial='111')
+    state = forms.ChoiceField(choices=area_list)
+    revenue = forms.IntegerField(min_value=0, max_value=999999999999, initial=0)
+    employees = forms.IntegerField(min_value=0, max_value=999999, initial=0)
+    wages_annual = forms.IntegerField(min_value=0, max_value=999999999999, initial=0)
+    emp_based_lc = forms.BooleanField(required=False)
+    wage_based_lc = forms.BooleanField(required=False)
--- compact/compactapp/views.py
+++ compact/compactapp/views.py
@@ -3,33 +3,25 @@ from django.shortcuts import render
 from django.template import Context, loader, Template
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from compactapp.models import Area
+from .forms import SimpleAnalysis
+from .forms import AdvancedAnalysis
 
 import xlrd
-import itertools
+
 
 # Create your views here.
 def home(request):
     template = loader.get_template('base.html')
-    areas = Area.objects.values('name')
-    area_list = list(entry['name'] for entry in areas)
+    simple_analysis = SimpleAnalysis()
+    advanced_analysis = AdvancedAnalysis()
 
-    industry_file_location = "FinalNAICStoIOMatch.xlsx"
-    workbook = xlrd.open_workbook(industry_file_location)
-    sheet = workbook.sheet_by_name('NAICS to Sector')
-    sector_ID = list(item for item in sheet.col_values(0))
-    sector_description = list(item for item in sheet.col_values(1))
-    del sector_description[0]
-    sector_NAICS = list(item for item in sheet.col_values(3))
-    del sector_NAICS[0]
-    del sector_NAICS[-1]
-    industries = ["{} - NAICS Code: {}".format(desc, int(NAICS)) for desc, NAICS in zip(sector_description, sector_NAICS)]
-
-    context = {'areas': areas, 'industries': industries}
-
-    return render(request, 'base.html', context)
+    return render(request, 'base.html', {'simple_analysis': simple_analysis, 'advanced_analysis': advanced_analysis})
 
 def results(request):
+
     template = loader.get_template('results.html')
+
+
     return HttpResponse(template.render())
 
 def error_page(request):
